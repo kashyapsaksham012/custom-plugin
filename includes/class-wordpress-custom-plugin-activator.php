e98049a148a4f5cbb924716b3e5de21ceff4c932
kashyapsaksham012 <kashyapsaksham012@gmail.com>
2026-01-08 12:26:10 +0530

    Initial commit: Custom WordPress Plugin

---
+++ includes/class-wordpress-custom-plugin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://saksham.com/
+ * @since      1.0.0
+ *
+ * @package    Wordpress_Custom_Plugin
+ * @subpackage Wordpress_Custom_Plugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wordpress_Custom_Plugin
+ * @subpackage Wordpress_Custom_Plugin/includes
+ * @author     Saksham <me@saksham.com>
+ */
+class Wordpress_Custom_Plugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wordpress-custom-plugin-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://saksham.com/
+ * @since      1.0.0
+ *
+ * @package    Wordpress_Custom_Plugin
+ * @subpackage Wordpress_Custom_Plugin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wordpress_Custom_Plugin
+ * @subpackage Wordpress_Custom_Plugin/includes
+ * @author     Saksham <me@saksham.com>
+ */
+class Wordpress_Custom_Plugin_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
